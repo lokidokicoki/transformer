54686e4928e84e0769b490b5199b0dca0746348b
rtregaskis <rtregaskis@dresources.com>
2019-04-01 20:31:43 +0100

    feat: include services and fillups. TODO: convert structure

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as uuid from 'short-uuid';
 const inputFile = 'records.json';
 const outputFile = 'jalopy.json';
 
-let records: { vehicles: { [x: string]: any; }; };
+let records: { vehicles: { [x: string]: any; }; services: { [x: string]: any; }; fillUps: { [x: string]: any; }; };
 
 let vehicles = new Map();
 let services = new Map();
@@ -14,11 +14,23 @@ async function main() {
   records = await fs.readJson(inputFile);
 
   for (const key of Object.keys(records.vehicles)) {
-    const vehicle = records.vehicles[key];
-    //console.log(vehicle)
-    vehicle.id = uuid.generate();
-    vehicles.set(vehicle.id, vehicle);
+    const record = records.vehicles[key];
+    record.id = uuid.generate();
+    vehicles.set(record.id, record);
   }
+
+  for (const key of Object.keys(records.services)) {
+    const record = records.services[key];
+    record.id = uuid.generate();
+    services.set(record.id, record);
+  }
+  for (const key of Object.keys(records.fillUps)) {
+    const record = records.fillUps[key];
+    record.id = uuid.generate();
+    fillUps.set(record.id, record);
+  }
+
+
 
   let output = {
     vehicles: [...vehicles],
