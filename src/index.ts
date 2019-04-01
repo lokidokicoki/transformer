bf68c88f79152fda2c8240d628a3754324579cc4
rtregaskis <rtregaskis@dresources.com>
2019-04-01 20:26:54 +0100

    add tslint file, clean up

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,32 @@
 import * as fs from 'fs-extra';
-import * as path from 'path';
 import * as uuid from 'short-uuid';
 
 const inputFile = 'records.json';
 const outputFile = 'jalopy.json';
 
-let records;
+let records: { vehicles: { [x: string]: any; }; };
 
-let    vehicles = new Map();
+let vehicles = new Map();
 let services = new Map();
 let fillUps = new Map();
 
-async function main(){
-    records = await fs.readJson(inputFile);
+async function main() {
+  records = await fs.readJson(inputFile);
 
-    for(const key of Object.keys(records.vehicles)){
-        const vehicle = records.vehicles[key];
-        //console.log(vehicle)
-        vehicle.id = uuid.generate();
-        vehicles.set(vehicle.id, vehicle);
+  for (const key of Object.keys(records.vehicles)) {
+    const vehicle = records.vehicles[key];
+    //console.log(vehicle)
+    vehicle.id = uuid.generate();
+    vehicles.set(vehicle.id, vehicle);
+  }
 
-    }
+  let output = {
+    vehicles: [...vehicles],
+    services: [...services],
+    fillUps: [...fillUps]
+  }
 
-   let output = {
-        vehicles : [...vehicles],
-
-    }
-    console.log(output.vehicles)
-
-
-    
-
-    await fs.writeJson(outputFile, output);
+  await fs.writeJson(outputFile, output, { spaces: 2 });
 }
 
 
